b205a07ad51192ea9f2313c1d22abfcc207c108b
pvl1175 <vitaly.popov.75@gmail.com>
2019-06-24 15:19:46 +0300

    update

--- build64.sh
+++ build64.sh
@@ -7,7 +7,7 @@ rm -r -f api
 
 git clone https://github.com/pvl1175/thrift-interface-definitions
 mkdir api
-thrift -r -gen cpp:no_skeleton,pure_enums,moveable_types -out .\api .\thrift-interface-definitions\lookfor9.thrift
+thrift -r -gen cpp:no_skeleton,pure_enums,moveable_types -out ./api ./thrift-interface-definitions/lookfor9.thrift
 
 git clone https://github.com/pvl1175/thrift
 cd thrift
